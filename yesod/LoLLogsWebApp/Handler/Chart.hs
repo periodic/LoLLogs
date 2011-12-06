d6cae83ef8df7a414ba82c9e92aa5fbce65230c6
Eric Stratmann <estrat@cs.stanford.edu>
2011-12-05 22:24:28 -0800

    Replaced Chart package with Flot. Included basic proof of concept plot

--- yesod/LoLLogsWebApp/Handler/Chart.hs
+++ yesod/LoLLogsWebApp/Handler/Chart.hs
@@ -4,28 +4,57 @@
 module Handler.Chart where
 
 import Import
-import Graphics.Rendering.Chart.Simple
 import Data.Map as M
 
 getGameStatsR :: Handler RepHtml
 getGameStatsR = do
     games <- runDB $ selectList [] [Asc GameLength]
     let bins = bin binBy10Mins games 0 10
-    _ <- liftIO $ plotPNG file (xs bins) (ys bins)
+    let dataPoints = fmap (\(len,cBin) -> (show len, length cBin)) bins
+    let chart = createBarChart dataPoints
     defaultLayout $ do
         setTitle "Game Len Analysis"
         $(widgetFile "game-len")
 
 -- Should probably move this elsewhere
 
-file :: String
-file = "static/img/chart-generated.png"
+createBarChart :: [(String, Int)] -> Widget
+createBarChart ds = do
+    let ys = getYs ds
+    let opts = "{xaxis: {ticks: " ++ getXs ds ++ "}, series: {bars: {show: true}}}"
+    callFlot ys opts
 
-xs :: [(Int, [(a,Game)])] -> [Double]
-xs = fmap (fromIntegral . fst)
 
-ys :: [(Int, [(a,Game)])] -> [Double]
-ys = fmap (fromIntegral . length . snd)
+getYs :: [(String, Int)] -> String
+getYs d = fmap replaceParens . show $ getYs' (0::Int)  d
+  where
+    getYs' i ((_,v):rest) = (i, v) : getYs' (i+1) rest
+    getYs' _ [] = []
+
+getXs :: [(String, Int)] -> String
+getXs d = fmap replaceParens . show $  getXs' (0::Int) d
+  where
+    getXs' i ((c,_):rest) = (fromIntegral i + 0.5 :: Double, c) : getXs' (i+1) rest
+    getXs' _ [] = []
+
+-- Needed because flot uses arrays instead of tuples for pairs
+replaceParens :: Char -> Char
+replaceParens c | c == '(' = '['
+                | c == ')' = ']'
+                | otherwise = c
+
+callFlot :: String -> String -> Widget
+callFlot plots opts = do
+    addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+    -- todo use static route for this
+    addScriptRemote "/static/js/jquery.flot.min.js"
+    chartId <- lift newIdent
+    toWidget [hamlet|<div id="#{chartId}" style="width:600px;height:300px;">|]
+    toWidget [julius|
+        $(function() {
+            $.plot($('##{chartId}'), [#{plots}], #{opts});
+        })
+    |]
 
 getGameLen :: (a, Game) -> Int
 getGameLen = gsgameLength . gameGameStats . snd
